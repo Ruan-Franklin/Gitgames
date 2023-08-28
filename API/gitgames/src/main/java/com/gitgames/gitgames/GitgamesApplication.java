889e44cda88f1edc6168c28af0414746a59ac1cb
Ruan Franklin <94229859+Ruan-Franklin@users.noreply.github.com>
2023-08-28 19:50:13 -0300

    :heavy_plus_sign: build: Adiciona dependências iniciais do projeto

---
+++ API/gitgames/src/main/java/com/gitgames/gitgames/GitgamesApplication.java
@@ -0,0 +1,13 @@
+package com.gitgames.gitgames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitgamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitgamesApplication.class, args);
+	}
+
+}
---
+++ API/gitgames/src/test/java/com/gitgames/gitgames/GitgamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitgames.gitgames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitgamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
