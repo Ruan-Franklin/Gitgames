6162f6a08bfda69c007559ef60c2c33f80e28c44
Ruan Franklin <94229859+Ruan-Franklin@users.noreply.github.com>
2023-09-14 10:48:42 -0300

    :sparkles: Adiciona criação de produto

---
+++ API/gitgames/src/main/java/com/gitgames/gitgames/domain/produto/Produto.java
@@ -0,0 +1,34 @@
+package com.gitgames.gitgames.domain.produto;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Table(name = "produto")
+@Entity(name = "produto")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+    private String descricao;
+    private BigDecimal preco;
+    private String plataforma;
+    private Integer estoque;
+
+    public Produto(RequestProduto requestProduto){
+        this.nome = requestProduto.nome();
+        this.descricao = requestProduto.descricao();
+        this.preco = requestProduto.preco();
+        this.plataforma = requestProduto.plataforma();
+        this.estoque = requestProduto.estoque();
+    }
+
+
+}
