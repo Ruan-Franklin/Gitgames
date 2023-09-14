70ad7715f35d93abb90cf9c2aba558f9bb8ceaa2
ruan-franklin <ruanfranklinmengo@gmail.com>
2023-09-14 10:51:38 -0300

    :sparkles: Adiciona criação de produto

---
+++ API/gitgames/src/main/java/com/gitgames/gitgames/repositories/ProdutoRepository.java
@@ -0,0 +1,7 @@
+package com.gitgames.gitgames.repositories;
+import com.gitgames.gitgames.domain.produto.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProdutoRepository  extends JpaRepository<Produto, String>{
+    
+}
